456e737db97557cb99f2c7ec836f96c5158ff1d7
sourav-18 <souravhd2@gmail.com>
2026-04-22 02:54:58 +0530

    message

--- src/main/java/com/ms/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/ms/api_gateway/ApiGatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
